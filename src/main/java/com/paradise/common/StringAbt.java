92eb7d76caa4b7baaf3e44d349b2aac45070a9dd
xiumu <zdparadise@icloud.com>
2018-08-03 09:45:19 +0800

    :construction:

---
+++ src/main/java/com/paradise/common/StringAbt.java
@@ -0,0 +1,23 @@
+/**
+ * <b>工程名：</b>ZhangDong<br/>
+ * <b>包  名：</b>com.paradise.common<br/>
+ * <b>文件名：</b>StringAbt.java<br/>
+ * <b>日  期：</b>2018/8/2<br/>
+ * <b>Copyright (c)</b> 2018 梯升-版权所有<br/>
+ */
+
+package com.paradise.common;
+
+/**
+ * <b>类  名：</b>StringAbt<br/>
+ * <b>类描述：</b><br/>
+ * <b>创建人：</b>Administrator<br/>
+ * <b>创建时间：</b>2018/8/2<br/>
+ * <b>修改人：</b><br/>
+ * <b>修改时间：</b><br/>
+ * <b>修改备注：</b><br/>
+ *
+ * @version 1.0.0 <br/>
+ */
+public class StringAbt {
+}
